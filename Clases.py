0342b0c241020522e8ab2915ba1bb13138b7d7aa
Carla Vargas <carlav02@gmail.com>
2024-08-19 23:01:07 -0400

    confirmacion

---
+++ Clases.py
@@ -0,0 +1,20 @@
+class Persona():
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def hablar(self):
+        print(f"{self.nombre} esta hablando")
+    def caminar(self):
+        print(f"{self.nombre} esta caminando")
+
+persona1 = Persona("Cristina", "Torres", 23)
+persona2 = Persona("Cristian", "Hada", 20)
+
+print(f"{persona1.nombre}")
+print(f"{persona1.apellido}")
+print(f"{persona1.edad}")
+
+persona1.hablar
+persona2.caminar
